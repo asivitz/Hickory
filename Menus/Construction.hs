413d00e29b259a9b38aca0cd66da002e1184066e
Axis Sivitz <aosivitz@gmail.com>
2014-08-07 16:27:21 -0400

    Add some menu building utils

---
+++ Menus/Construction.hs
@@ -0,0 +1,21 @@
+module Menus.Construction where
+
+import Utils.Utils
+import Types.Types
+
+data Interval a = Interval a a
+
+intervalIndices :: [Double]
+intervalIndices = [5, 3, 1, 4, 3, 0]
+
+intervals :: [Interval Double]
+intervals = map (\i -> Interval (i * 0.1) (i * 0.1 + 0.5)) intervalIndices
+
+pickInterval idx = intervals !! (idx `mod` 6)
+
+constrainInterval :: Double -> Int -> Double
+constrainInterval fraction idx = case (pickInterval idx) of
+                                     Interval low high -> rlerpClamp fraction low high
+
+slide True (RPos frac offset) = (RPos (frac * 0.5) offset)
+slide False (RPos frac offset) = (RPos ((2 - frac) * 0.5) offset)
