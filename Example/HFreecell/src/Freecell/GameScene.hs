59b99a8931888f94ae7e9187a448f90d8a9712d4
Axis Sivitz <aosivitz@gmail.com>
2014-09-11 16:11:29 -0400

    Extract out game scene

---
+++ Example/HFreecell/src/Freecell/GameScene.hs
@@ -0,0 +1,84 @@
+module Freecell.GameScene (makeScene, InputEvent(..)) where
+
+import FreeCell
+import Engine.Input
+import Engine.Component
+import Math.Matrix
+import Math.VectorMatrix
+import Engine.Model
+import Engine.Run
+import Systems.Draw
+import Engine.CompUtils
+import Types.Color
+import Types.Types
+import Data.IORef
+import Data.Tuple
+import Graphics.Drawing
+import Utils.Utils
+import Control.Monad
+import Control.Lens
+import Camera.Camera
+import qualified Systems.DrawState as DrawState
+import Math.Vector
+
+data Resources = Resources {
+               solidShader :: Maybe Shader
+               }
+
+data GameModel = GameModel {
+               board :: Board
+               }
+
+data InputEvent = RawEvent RawInput
+                | NewGame deriving Show
+
+loadResources :: String -> IO Resources
+loadResources path = do
+        solid <- loadShader path "Shader.vsh" "SolidColor.fsh"
+        return $ Resources solid
+
+render :: Resources -> Model ComponentStore GameModel -> IO ()
+render (Resources solidShader) model = do
+        {-print $ "Rendering model: " ++ (show model)-}
+
+        whenMaybe solidShader $ \sh -> do
+            let ds = getModelComponents drawStates model
+            forM_ (stripEnts ds) $ \(DrawState pos) ->
+                drawSpec pos uiLabel (SolidSquare (Size 50 50) white sh)
+
+spawnThing pos = do
+        e <- spawnEntity
+        addComp e drawStates $ DrawState pos
+        addComp e mouseDrags $ MouseDrag (v3 0 0 0)
+        {-addComp components e newtonianMovers $ NewtonianMover (v3 40 16 0) (v3 0 0 0)-}
+        return []
+
+processInput :: RenderInfo -> InputEvent -> Model ComponentStore GameModel -> (Model ComponentStore GameModel, [InputEvent])
+processInput (RenderInfo mat ss _) (RawEvent (InputTouchDown pos pid)) model = swap $ runModel (spawnThing p') model
+    where p' = lerpUnproject pos 5 mat (viewportFromSize ss)
+
+processInput (RenderInfo mat ss _) (RawEvent (InputTouchLoc pos pid)) model = 
+        (over components (\cs -> upComps2 cs drawStates mouseDrags (DrawState.snapToMouse p')) model, [])
+    where p' = lerpUnproject pos 5 mat (viewportFromSize ss)
+
+processInput (RenderInfo mat ss _) NewGame model = (model, [])
+        {-mapM_ (\c -> spawnCard fcgame (v3 0 0 (-5)) c) (allCards board)-}
+
+processInput _ _ model = (model, [])
+
+stepComponents :: Double -> ComponentStore -> ComponentStore
+stepComponents delta cs = upComps2 cs drawStates newtonianMovers (DrawState.upDS delta)
+
+makeScene = do
+        board <- makeGame
+        is <- newIORef (Input [])
+        let cam = Camera (Ortho 800 (-20) 1) (Route pZero Nothing)
+            scene = Scene {
+                          _model = (newModel cam emptyComponentStore (GameModel board)),
+                          _renderInfo = RenderInfo mat44Identity nullSize uiLabel,
+                          _loadResources = loadResources "Example/HFreecell/resources",
+                          _stepModel = makeStepModel processInput stepComponents,
+                          _render = render,
+                          _inputStream = is,
+                          _loadedRender = Nothing }
+        return scene
--- Example/HFreecell/src/main.hs
+++ Example/HFreecell/src/main.hs
@@ -1,91 +1,17 @@
 {-# LANGUAGE NamedFieldPuns #-}
  
 import Engine.Run
-import Engine.Model
-import Engine.Component
-import Types.Types
-import Math.Vector
-import Systems.Draw
-import qualified Systems.DrawState as DrawState
-import Types.Color
-import Utils.Utils
 import Engine.Input
-import Engine.CompUtils
-import Math.VectorMatrix
-import Control.Lens
-import Control.Monad
-import Camera.Camera
-import Graphics.Drawing
-import FreeCell
-import Data.IORef
-import Data.Tuple
-import Math.Matrix
+import qualified Freecell.GameScene as GameScene
 
 {-import qualified Freecell.Systems.Game as FCGame-}
 {-import qualified Freecell.Systems.Menu as FCMenu-}
 {-import qualified Freecell.Systems.Draw as FCDraw-}
 
-data Resources = Resources {
-               solidShader :: Maybe Shader
-               }
-
-data GameModel = GameModel {
-               board :: Board
-               }
-
-data InputEvent = RawEvent RawInput
-                | NewGame deriving Show
-
-loadResources :: String -> IO Resources
-loadResources path = do
-        solid <- loadShader path "Shader.vsh" "SolidColor.fsh"
-        return $ Resources solid
-
-render :: Resources -> Model ComponentStore GameModel -> IO ()
-render (Resources solidShader) model = do
-        {-print $ "Rendering model: " ++ (show model)-}
-
-        whenMaybe solidShader $ \sh -> do
-            let ds = getModelComponents drawStates model
-            forM_ (stripEnts ds) $ \(DrawState pos) ->
-                drawSpec pos uiLabel (SolidSquare (Size 50 50) white sh)
-
-spawnThing pos = do
-        e <- spawnEntity
-        addComp e drawStates $ DrawState pos
-        addComp e mouseDrags $ MouseDrag (v3 0 0 0)
-        {-addComp components e newtonianMovers $ NewtonianMover (v3 40 16 0) (v3 0 0 0)-}
-        return []
-
-processInput :: RenderInfo -> InputEvent -> Model ComponentStore GameModel -> (Model ComponentStore GameModel, [InputEvent])
-processInput (RenderInfo mat ss _) (RawEvent (InputTouchDown pos pid)) model = swap $ runModel (spawnThing p') model
-    where p' = lerpUnproject pos 5 mat (viewportFromSize ss)
-
-processInput (RenderInfo mat ss _) (RawEvent (InputTouchLoc pos pid)) model = 
-        (over components (\cs -> upComps2 cs drawStates mouseDrags (DrawState.snapToMouse p')) model, [])
-    where p' = lerpUnproject pos 5 mat (viewportFromSize ss)
-
-processInput (RenderInfo mat ss _) NewGame model = (model, [])
-        {-mapM_ (\c -> spawnCard fcgame (v3 0 0 (-5)) c) (allCards board)-}
-
-processInput _ _ model = (model, [])
-
-stepComponents :: Double -> ComponentStore -> ComponentStore
-stepComponents delta cs = upComps2 cs drawStates newtonianMovers (DrawState.upDS delta)
 
 main :: IO ()
 main = do
-        board <- makeGame
-        is <- newIORef (Input [])
-        let cam = Camera (Ortho 800 (-20) 1) (Route pZero Nothing)
-            scene = Scene {
-                          _model = (newModel cam emptyComponentStore (GameModel board)),
-                          _renderInfo = RenderInfo mat44Identity nullSize uiLabel,
-                          _loadResources = loadResources "Example/HFreecell/resources",
-                          _stepModel = makeStepModel processInput stepComponents,
-                          _render = render,
-                          _inputStream = is,
-                          _loadedRender = Nothing }
                           
-        glfwMain [scene]
-                 RawEvent
+        scenes <- sequence [GameScene.makeScene]
+        glfwMain scenes
+                 GameScene.RawEvent
