da90e6c4c768e583955ed7a521ffa89b8c2316ac
Axis Sivitz <aosivitz@gmail.com>
2014-10-19 14:28:15 -0400

    IOS run function

--- iOS/IOS/Run.hs
+++ iOS/IOS/Run.hs
@@ -3,22 +3,31 @@
 module IOS.Run where
 
 import Graphics.Rendering.OpenGL.Raw.Core31
-import Graphics.Rendering.OpenGL.Raw.ARB.GeometryShader4
 import Data.Bits
 import Graphics.Drawing
 import Engine.Scene.Scene
 import Engine.Scene.Run
 import Types.Types
-
-import Foreign.Ptr
-import Control.Concurrent
-import Control.Monad
+import Data.Time
 import Data.IORef
-import Foreign
+
 import Foreign.C
 
-iosInitEx :: [SceneOperator ie] -> CInt -> CInt -> IO ()
-iosInitEx ops w h = iosInit (Size (fromIntegral w) (fromIntegral h)) ops
+makeIOSStepFunc :: Show ie => [SceneOperator ie] -> IO (IO ())
+makeIOSStepFunc operators = do
+        start_time <- getCurrentTime
+        timeref <- newIORef start_time
+
+        return $ do
+            prev_time <- readIORef timeref
+            current_time <- getCurrentTime
+            let delta = min 0.1 $ realToFrac (diffUTCTime current_time prev_time)
+            writeIORef timeref current_time
+
+            runOneFrame (return ()) operators iosRender delta
+
+iosInitFunc :: [SceneOperator ie] -> CInt -> CInt -> IO ()
+iosInitFunc ops w h = iosInit (Size (fromIntegral w) (fromIntegral h)) ops
 
 iosInit :: Size Int -> [SceneOperator ie] -> IO ()
 iosInit scrSize operators = do
@@ -26,7 +35,6 @@ iosInit scrSize operators = do
         glClearColor 0.125 0.125 0.125 1
         glBlendFunc gl_SRC_ALPHA gl_ONE_MINUS_SRC_ALPHA
         glActiveTexture gl_TEXTURE0
-        print scrSize
         
         {-glEnable gl_PROGRAM_POINT_SIZE -- for OSX-}
 
