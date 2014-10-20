f01bea1fc9816da8a2355f9adac6922c5c68e127
Axis Sivitz <aosivitz@gmail.com>
2014-10-20 15:07:13 -0400

    Add unprojectDelta function for unprojecting mouse movement

--- Utils/Projection.hs
+++ Utils/Projection.hs
@@ -9,3 +9,8 @@ import Engine.Scene.Scene
 
 unproject :: V2 -> Scalar -> RenderInfo -> V3
 unproject pos z (RenderInfo mat ss _) = lerpUnproject pos z mat (viewportFromSize ss)
+
+-- Useful for transforming a difference in touch coordinates into
+-- a world difference vector
+unprojectDelta :: V2 -> Scalar -> RenderInfo -> V3
+unprojectDelta p depth renderinfo = (unproject p depth renderinfo) - (unproject pZero depth renderinfo)
