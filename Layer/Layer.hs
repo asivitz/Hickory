80b0906d715fe874d9d029dbd9b12be9f64019f8
Axis Sivitz <aosivitz@gmail.com>
2016-06-14 12:42:34 -0400

    Debug layer

--- Layer/Layer.hs
+++ Layer/Layer.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Layer.Layer where
 
 import Data.List
@@ -16,3 +18,38 @@ constructLayer inputf stepf nextLayer (lay1, lay2) msg1s =
 constructStatelessLayer :: (lay2 -> msg1 -> [msg2]) -> Layer lay2 msg2 -> Layer (lay1, lay2) msg1
 constructStatelessLayer inputf =
         constructLayer (\lay2 ui msg1 -> (ui, inputf lay2 msg1)) (const id)
+
+-- Debug Layer
+data DebugMsg = FlipDebug
+              | StepForward
+              | JumpForward
+              | StepBackward
+              | JumpBackward
+
+data DebugState model = DebugState {
+             debug :: Bool,
+             modelStack :: [model],
+             stackIndex :: Int
+             }
+             deriving (Show)
+
+mkDebugState = DebugState False [] 0
+
+debugInput :: b -> DebugState b -> DebugMsg -> DebugState b
+debugInput model debugstate@DebugState { debug, stackIndex, modelStack } input = state'
+        where state' = case input of
+                         FlipDebug -> if debug
+                                          then debugstate { debug = not debug, stackIndex = 0, modelStack = drop stackIndex modelStack }
+                                          else debugstate { debug = not debug, stackIndex = 0 }
+                         StepForward -> debugstate { stackIndex = max 0 (stackIndex - 1) }
+                         JumpForward -> debugstate { stackIndex = max 0 (stackIndex - 10) }
+                         StepBackward -> debugstate { stackIndex = min (length modelStack - 1) (max 0 (stackIndex + 1)) }
+                         JumpBackward -> debugstate { stackIndex = min (length modelStack - 1) (max 0 (stackIndex + 10)) }
+
+debugLayer :: Layer b msg -> Layer (DebugState b, b) DebugMsg
+debugLayer subLayer (debugstate@DebugState { debug, modelStack, stackIndex }, model) debugmsgs = ret
+        where uis' = foldl' (debugInput model) debugstate debugmsgs
+              mdl' = subLayer model []
+              ret = if debug
+                          then (uis', modelStack !! stackIndex)
+                          else (uis' { modelStack = mdl' : (if length modelStack > 10000 then take 10000 modelStack else modelStack) }, mdl')
