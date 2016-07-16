fd6e6d0d9ceaf6c188e02eb57faaa726e3fdd3eb
Axis Sivitz <aosivitz@gmail.com>
2016-07-15 23:29:32 -0400

    Pre-calculate animation matrices

--- Graphics/Animation.hs
+++ Graphics/Animation.hs
@@ -1,12 +1,13 @@
 module Graphics.Animation where
 
 import Math.Vector
+import Math.Matrix
 import Linear.Quaternion
 import Data.List
 import qualified Utils.BVH as BVH
 
-data Frame a = Joint String (V3 a) (Quaternion a) [Frame a]
-             | End (V3 a)
+data Frame a = Joint String (M44 a) [Frame a]
+             | End (M44 a)
              deriving (Show)
 
 data Animation a = Animation {
@@ -20,12 +21,19 @@ degToRad = (*(pi/180))
 
 bvhToAnimation :: BVH.BVH -> Animation Double
 bvhToAnimation (BVH.BVH joint (BVH.Motion nFrames fTime fs)) =
-        Animation nFrames fTime (map (\f -> let ([], frame) = animateJoint f joint in frame) fs)
+        Animation nFrames fTime (map (\f -> let ([], frame) = animateJoint (mkTransformation rot zero) f joint in frame) fs)
+    where modelUp = unit _y
+          worldUp = unit _z
+          axis = cross modelUp worldUp
+          angle = vabsangle worldUp modelUp
+          rot = axisAngle axis angle
 
-animateJoint :: [Double] -> BVH.Joint -> ([Double], Frame Double)
-animateJoint (zr:yr:xr:rest) (BVH.Joint 3 name (x,y,z) children) = (leftOverChannels, Joint name (V3 x y z) (axisAngle (unit _z) (degToRad zr) * axisAngle (unit _y) (degToRad yr) * axisAngle (unit _x) (degToRad xr)) childJoints)
-    where (leftOverChannels, childJoints) = mapAccumL animateJoint rest children
-animateJoint (xp:yp:zp:zr:yr:xr:rest) (BVH.Joint 6 name (x,y,z) children) = (leftOverChannels, Joint name (zero) (axisAngle (unit _z) (degToRad zr) * axisAngle (unit _y) (degToRad yr) * axisAngle (unit _x) (degToRad xr)) childJoints)
-    where (leftOverChannels, childJoints) = mapAccumL animateJoint rest children
-animateJoint rest (BVH.JointEnd (x,y,z)) = (rest, End (V3 x y z))
-animateJoint _ _ = error "Couldn't convert BVH to animation"
+animateJoint :: M44 Double -> [Double] -> BVH.Joint -> ([Double], Frame Double)
+animateJoint parentMat (zr:yr:xr:rest) (BVH.Joint 3 name (x,y,z) children) = (leftOverChannels, Joint name mat childJoints)
+    where (leftOverChannels, childJoints) = mapAccumL (animateJoint mat) rest children
+          mat = parentMat !*! mkTransformation (axisAngle (unit _z) (degToRad zr) * axisAngle (unit _y) (degToRad yr) * axisAngle (unit _x) (degToRad xr)) (V3 x y z)
+animateJoint parentMat (xp:yp:zp:zr:yr:xr:rest) (BVH.Joint 6 name (x,y,z) children) = (leftOverChannels, Joint name mat childJoints)
+    where (leftOverChannels, childJoints) = mapAccumL (animateJoint mat) rest children
+          mat = parentMat !*! mkTransformation (axisAngle (unit _z) (degToRad zr) * axisAngle (unit _y) (degToRad yr) * axisAngle (unit _x) (degToRad xr)) zero
+animateJoint parentMat rest (BVH.JointEnd (x,y,z)) = (rest, End $ parentMat !*! mkTranslation (V3 x y z))
+animateJoint _ _ _ = error "Couldn't convert BVH to animation"
