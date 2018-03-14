2faeefc552c69dd905cb804a58ed72b4932f225c
Axis Sivitz <aosivitz@gmail.com>
2018-03-14 19:15:42 -0400

    Game logic needs access to framebuffer size
    If game logic sets its own framebuffer callback, it overwrites the input
    one. So we just provide a better API so that we don't need another
    callback

--- GLFW/Platforms/GLFW.hs
+++ GLFW/Platforms/GLFW.hs
@@ -1,9 +1,15 @@
 module Platforms.GLFW where
 
-import qualified Graphics.UI.GLFW as GLFW
-import qualified Platforms.GLFW.Bridge as Bridge
+import Data.IORef (IORef)
 import Hickory.Input
 import Hickory.Platform
+import Hickory.Types (Size)
+import qualified Graphics.UI.GLFW as GLFW
+import qualified Platforms.GLFW.Bridge as Bridge
 
-makeGLFWInputPoller :: GLFW.Window -> IO (IO [RawInput])
-makeGLFWInputPoller win = makeInputPoller (Bridge.setupInput win)
+makeGLFWInputPoller :: GLFW.Window -> IORef (Size Int) -> IO (IO [RawInput])
+makeGLFWInputPoller win fbSizeRef = makeInputPoller (Bridge.setupInput win fbSizeRef)
+
+
+getGLFWBufferSizeRef :: GLFW.Window -> IO (IORef (Size Int))
+getGLFWBufferSizeRef = Bridge.getBufferSizeRef
--- GLFW/Platforms/GLFW/Bridge.hs
+++ GLFW/Platforms/GLFW/Bridge.hs
@@ -15,99 +15,99 @@ import Control.Monad
 --TODO: RawInput Int should instead use a generic engine key type, and then
     --a method of converting GLFW.Key to it
 
-data SysData = SysData {
-             touches :: HashMap.HashMap Int UTCTime,
-             keys :: HashMap.HashMap Key UTCTime,
-             fbSize :: Size Int
-            }
+data SysData = SysData
+  { touches :: HashMap.HashMap Int UTCTime
+  , keys :: HashMap.HashMap Key UTCTime
+  }
 
-empty = SysData {
-                touches = HashMap.empty,
-                keys = HashMap.empty,
-                fbSize = nullSize
-                }
+empty = SysData
+  { touches = HashMap.empty
+  , keys = HashMap.empty
+  }
 
+getBufferSizeRef :: GLFW.Window -> IO (IORef (Size Int))
+getBufferSizeRef win = do
+  fbSize     <- uncurry Size <$> GLFW.getFramebufferSize win
+  fbSizeRef  <- newIORef fbSize
+  GLFW.setFramebufferSizeCallback win (Just $ \_ w h -> print "resize" >> print (w,h) >> writeIORef fbSizeRef (Size w h))
+  pure fbSizeRef
 
-setupInput win addInput = do
-        (width, height) <- GLFW.getFramebufferSize win
-        sd <- newIORef (SysData HashMap.empty HashMap.empty (Size width height))
-        GLFW.setMouseButtonCallback win $ Just (mouseButtonCallback sd addInput)
-        GLFW.setKeyCallback win $ Just (keyCallback sd addInput)
-        GLFW.setFramebufferSizeCallback win $ Just (framebufferSizeCallback sd)
-        return $ stepInput sd win addInput
+setupInput :: GLFW.Window -> IORef (Size Int) -> (RawInput -> IO ()) -> IO (IO ())
+setupInput win fbSizeRef addInput = do
+  sd <- newIORef (SysData HashMap.empty HashMap.empty)
+  GLFW.setMouseButtonCallback win $ Just (mouseButtonCallback sd fbSizeRef addInput)
+  GLFW.setKeyCallback win $ Just (keyCallback sd addInput)
+  pure $ stepInput sd fbSizeRef win addInput
 
-stepInput sd win addInput = do
-        SysData { fbSize, touches, keys } <- readIORef sd
-        GLFW.pollEvents
-        curPos <- GLFW.getCursorPos win
-        let curloc = touchPosToScreenPos fbSize curPos
-            ident = fromMaybe 0 $ listToMaybe $ HashMap.keys touches
-        addInput (InputTouchesLoc [(curloc,ident)])
+stepInput :: IORef SysData -> IORef (Size Int) -> GLFW.Window -> (RawInput -> IO ()) -> IO ()
+stepInput sd fbSizeRef win addInput = do
+  SysData { touches, keys } <- readIORef sd
+  fbSize <- readIORef fbSizeRef
+  GLFW.pollEvents
+  curPos <- GLFW.getCursorPos win
+  let curloc = touchPosToScreenPos fbSize curPos
+      ident = fromMaybe 0 $ listToMaybe $ HashMap.keys touches
+  addInput (InputTouchesLoc [(curloc,ident)])
 
-        unless (HashMap.null keys) $ do
-                time <- getCurrentTime
-                let relative = HashMap.map (\t -> realToFrac (diffUTCTime time t)) keys
-                addInput (InputKeysHeld relative)
+  unless (HashMap.null keys) $ do
+    time <- getCurrentTime
+    let relative = HashMap.map (\t -> realToFrac (diffUTCTime time t)) keys
+    addInput (InputKeysHeld relative)
 
-framebufferSizeCallback :: IORef SysData -> GLFW.Window -> Int -> Int -> IO ()
-framebufferSizeCallback platform win width height = modifyIORef platform (\sd@SysData { fbSize } -> sd { fbSize = Size width height })
+mouseButtonCallback :: IORef SysData -> IORef (Size Int) -> (RawInput -> IO ()) -> GLFW.Window -> GLFW.MouseButton -> GLFW.MouseButtonState -> t -> IO ()
+mouseButtonCallback platform fbSizeRef addInput win button buttonState modkeys = do
+  fbSize <- readIORef fbSizeRef
+  sd@SysData { touches } <- readIORef platform
 
-mouseButtonCallback :: IORef SysData -> (RawInput -> IO ()) -> GLFW.Window -> GLFW.MouseButton -> GLFW.MouseButtonState -> t -> IO ()
-mouseButtonCallback platform addInput win button buttonState modkeys =
-        do
-            sd@SysData { fbSize, touches } <- readIORef platform
+  curPos <- GLFW.getCursorPos win
 
-            curPos <- GLFW.getCursorPos win
+  let pos = touchPosToScreenPos fbSize curPos
+      touchid = touchIdent button
 
-            let pos = touchPosToScreenPos fbSize curPos
-                touchid = touchIdent button
-            (ev, touches') <-
-                    case buttonState of
-                        GLFW.MouseButtonState'Pressed -> do
-                            time <- getCurrentTime
-                            return (InputTouchesDown [(pos,touchid)], HashMap.insert touchid time touches)
-                        GLFW.MouseButtonState'Released ->
-                            case HashMap.lookup touchid touches of
-                                Nothing -> return (InputTouchesUp [(0,pos,touchid)], touches)
-                                Just prev -> do
-                                    time <- getCurrentTime
-                                    let delta = realToFrac (diffUTCTime time prev)
-                                    return (InputTouchesUp [(delta,pos,touchid)], HashMap.delete touchid touches)
+  (ev, touches') <- case buttonState of
+    GLFW.MouseButtonState'Pressed -> do
+      time <- getCurrentTime
+      return (InputTouchesDown [(pos,touchid)], HashMap.insert touchid time touches)
+    GLFW.MouseButtonState'Released -> case HashMap.lookup touchid touches of
+      Nothing -> return (InputTouchesUp [(0,pos,touchid)], touches)
+      Just prev -> do
+        time <- getCurrentTime
+        let delta = realToFrac (diffUTCTime time prev)
+        return (InputTouchesUp [(delta,pos,touchid)], HashMap.delete touchid touches)
 
-            writeIORef platform sd { touches = touches' }
-            addInput ev
+  writeIORef platform sd { touches = touches' }
+  addInput ev
 
 keyCallback :: IORef SysData -> (RawInput -> IO ()) -> GLFW.Window -> GLFW.Key -> Int -> GLFW.KeyState -> GLFW.ModifierKeys -> IO ()
-keyCallback platform addInput win glfwkey scancode keyState modkeys =
-        do
-            sd@SysData { keys } <- readIORef platform
+keyCallback platform addInput win glfwkey scancode keyState modkeys = do
+  sd@SysData { keys } <- readIORef platform
 
-            time <- getCurrentTime
-            let key = toEnum (fromEnum glfwkey) -- only possible since our Key type is defined exactly the same as GLFW's
-                -- other platforms will need a more intelligent conversion
-                -- between key values
-            case keyState of
-                GLFW.KeyState'Pressed -> do
-                    addInput (InputKeyDown key)
-                    writeIORef platform sd { keys = HashMap.insert key time keys }
-                GLFW.KeyState'Released ->
-                    case HashMap.lookup key keys of
-                        Nothing -> addInput (InputKeyUp key 0)
-                        Just prev -> do
-                            addInput (InputKeyUp key (realToFrac (diffUTCTime time prev)))
-                            writeIORef platform sd { keys = HashMap.delete key keys }
-                _ -> return ()
+  time <- getCurrentTime
+  let key = toEnum (fromEnum glfwkey) -- only possible since our Key type is defined exactly the same as GLFW's
+      -- other platforms will need a more intelligent conversion
+      -- between key values
+  case keyState of
+    GLFW.KeyState'Pressed -> do
+      addInput (InputKeyDown key)
+      writeIORef platform sd { keys = HashMap.insert key time keys }
+    GLFW.KeyState'Released ->
+      case HashMap.lookup key keys of
+        Nothing -> addInput (InputKeyUp key 0)
+        Just prev -> do
+          addInput (InputKeyUp key (realToFrac (diffUTCTime time prev)))
+          writeIORef platform sd { keys = HashMap.delete key keys }
+    _ -> return ()
 
 touchIdent :: GLFW.MouseButton -> Int
 touchIdent button = case button of
-                        GLFW.MouseButton'1 -> 1
-                        GLFW.MouseButton'2 -> 2
-                        GLFW.MouseButton'3 -> 3
-                        GLFW.MouseButton'4 -> 4
-                        GLFW.MouseButton'5 -> 5
-                        GLFW.MouseButton'6 -> 6
-                        GLFW.MouseButton'7 -> 7
-                        GLFW.MouseButton'8 -> 8
+  GLFW.MouseButton'1 -> 1
+  GLFW.MouseButton'2 -> 2
+  GLFW.MouseButton'3 -> 3
+  GLFW.MouseButton'4 -> 4
+  GLFW.MouseButton'5 -> 5
+  GLFW.MouseButton'6 -> 6
+  GLFW.MouseButton'7 -> 7
+  GLFW.MouseButton'8 -> 8
 
 touchPosToScreenPos :: Size Int -> (Double, Double) -> V2 Scalar
 touchPosToScreenPos (Size w h) (x,y) = v2 (realToFrac x) (fromIntegral h - realToFrac y)
