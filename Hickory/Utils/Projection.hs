05e1db36523cafef8a41d6278722b8ff3d42ead6
Axis Sivitz <aosivitz@gmail.com>
2018-09-14 15:13:27 -0400

    Try to simplify interfaces to projection

--- Hickory/Utils/Projection.hs
+++ Hickory/Utils/Projection.hs
@@ -6,14 +6,15 @@ import Hickory.Math.Matrix
 import Hickory.Math.VectorMatrix
 import Hickory.Types
 import Hickory.Math.Vector
+import Control.Lens ((^.))
 
-unproject :: V2 Scalar -> Scalar -> Mat44 -> Size Int -> V3 Scalar
-unproject pos z mat ss = lerpUnproject pos z mat (fmap realToFrac $ viewportFromSize ss)
+unproject :: Mat44 -> Size Int -> Scalar -> V2 Scalar -> V2 Scalar
+unproject mat ss z pos = lerpUnproject pos z mat (fmap realToFrac $ viewportFromSize ss) ^. _xy
 
 -- Useful for transforming a difference in touch coordinates into
 -- a world difference vector
-unprojectDelta :: V2 Scalar -> Scalar -> Mat44 -> Size Int -> V3 Scalar
-unprojectDelta p depth mat ss = (unproject p depth mat ss) - (unproject zero depth mat ss)
+unprojectDelta :: V2 Scalar -> Scalar -> Mat44 -> Size Int -> V2 Scalar
+unprojectDelta p depth mat ss = (unproject mat ss depth p) - (unproject mat ss depth zero)
 
-viewport :: Real a => Size a -> V4 Scalar
-viewport (Size w h) = V4 0 0 (realToFrac w) (realToFrac h)
+project :: Mat44 -> Size Int -> V3 Scalar -> V3 Scalar
+project mat ss = viewProject mat (fmap realToFrac $ viewportFromSize ss)
