adaaeaf4933e139b5ae03b419bb104d22e635ade
Axis Sivitz <aosivitz@gmail.com>
2022-01-25 23:32:17 -0500

    Remove some (hopefully) unneeded stuff

--- check.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cabal build -v0 --ghc-options="-c -fforce-recomp -fno-code -no-link -O0" 2>&1 |  runhaskell parseCheck.hs
--- inst.rb
+++
@@ -1,7 +0,0 @@
-TRIPLE="aarch64-apple-darwin14"
-
-deps = `#{TRIPLE}-cabal install --user --dependencies-only --dry-run`.lines.drop(2).map{|l| l.split(" ")[0]}
-
-deps.each do |dep|
-    system("cabal-install-custom #{TRIPLE} #{dep}")
-end
