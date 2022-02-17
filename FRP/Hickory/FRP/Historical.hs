4332d27dd25e3e5252746cecb29156798c62f729
Axis Sivitz <aosivitz@gmail.com>
2022-02-16 23:48:46 -0500

    Can interpolate between data frames
    We can render at a high rate (say, 60fps) and only run game logic and
    physics at, say, 20 fps. This reduces CPU load but doesn't compromise
    visuals

--- FRP/Hickory/FRP/Historical.hs
+++ FRP/Hickory/FRP/Historical.hs
@@ -1,9 +1,12 @@
 module Hickory.FRP.Historical
   ( historical
   , historicalWithEvents
+  , historicalInterpolateWithEvents
   )
   where
 
+import Hickory.Math (Scalar)
+import Hickory.Math.Interpolate (Interpolatable(..))
 import Hickory.FRP.Combinators (unionFirst)
 import Reactive.Banana (Behavior, Event, MonadMoment, mapAccum)
 import qualified Data.Sequence as S
@@ -19,12 +22,29 @@ historicalWithEvents initial eStep eChangeIndex = do
                , changeIdx <$> eChangeIndex
                ]
   pure (uncurry S.index <$> history, evs)
- where
-  maxLen = 500
-  changeIdx delta (s, i) = ([], (s, min (max 0 (i + delta)) (S.length s - 1)))
-  append stepF (rest, i) =
-    let rst  = if i > 0 then S.drop i rest else rest
-        rst' = if not (null rst) then S.take maxLen rst else rst
-    in  case S.viewl rst' of
-          S.EmptyL -> error "Shouldn't happen"
-          x S.:< _ -> let (acc, evs) = stepF x in (evs, (acc S.<| rst', 0))
+
+historicalInterpolateWithEvents :: (MonadMoment m, Interpolatable a) => a -> Behavior Scalar -> Event (a -> (a,[b])) -> Event Int -> m (Behavior a, Event [b])
+historicalInterpolateWithEvents initial interp eStep eChangeIndex = do
+  (evs, history) <- mapAccum (S.singleton initial, 0) $
+    unionFirst [ append <$> eStep
+               , changeIdx <$> eChangeIndex
+               ]
+  pure (assemble <$> interp <*> history, evs)
+  where
+  assemble frac (s,i) = let from = S.index s (min (i + 1) (S.length s - 1))
+                            to = S.index s i
+                        in glerp frac from to
+
+maxLen :: Int
+maxLen = 500
+
+changeIdx :: Int -> (S.Seq a1, Int) -> ([a2], (S.Seq a1, Int))
+changeIdx delta (s, i) = ([], (s, min (max 0 (i + delta)) (S.length s - 1)))
+
+append :: Num b => (a1 -> (a1, a2)) -> (S.Seq a1, Int) -> (a2, (S.Seq a1, b))
+append stepF (rest, i) =
+  let rst  = if i > 0 then S.drop i rest else rest
+      rst' = if not (null rst) then S.take maxLen rst else rst
+  in  case S.viewl rst' of
+        S.EmptyL -> error "Shouldn't happen"
+        x S.:< _ -> let (acc, evs) = stepF x in (evs, (acc S.<| rst', 0))
--- core/Hickory/Math/Interpolate.hs
+++ core/Hickory/Math/Interpolate.hs
@@ -1,19 +1,27 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Hickory.Math.Interpolate where
 
 import Hickory.Math.Vector
+import Linear (V2,V3,V4)
+import Linear.Vector (lerp)
 
 nlerp :: Num a => a -> a -> a -> a
 nlerp fract a b = (a * (1 - fract)) + (b * fract)
 
 class Interpolatable i where
-        glerp :: Scalar -> i -> i -> i
+  glerp :: Scalar -> i -> i -> i
 
 instance Interpolatable Double where
-        glerp = nlerp
+  glerp = nlerp
+
 {-instance (Num a, Additive f) => Interpolatable (f a) where-}
         {-glerp = lerp-}
-{-instance Interpolatable Vector3 where-}
-        {-glerp = lerp-}
+
+instance Interpolatable (V2 Double) where glerp fr = flip (lerp fr)
+instance Interpolatable (V3 Double) where glerp fr = flip (lerp fr)
+instance Interpolatable (V4 Double) where glerp fr = flip (lerp fr)
+
 {-instance Interpolatable V4 where-}
         {-glerp = lerp-}
 
