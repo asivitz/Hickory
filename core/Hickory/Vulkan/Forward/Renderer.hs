4c27615bbd7c2c3f6dd5939175f902040c8a46cf
Axis Sivitz <aosivitz@gmail.com>
2022-12-14 14:47:05 -0500

    Fix blend ordering bugs. Seems to work well now

--- core/Hickory/Vulkan/Forward/Renderer.hs
+++ core/Hickory/Vulkan/Forward/Renderer.hs
@@ -10,7 +10,7 @@ import Hickory.Vulkan.ForwardRenderTarget (withPostProcessMaterial)
 import Linear (V4 (..), transpose, inv33, _m33, V2 (..), V3 (..), (!*!))
 import Hickory.Vulkan.Monad (FrameMonad, askFrameContext, material, BufferedUniformMaterial (..), cmdDrawBufferedMesh, getMeshes, addMesh, askDynamicMesh, useDynamicMesh, DynamicMeshMonad, textMesh)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Hickory.Vulkan.Types (PostConstants(..), RenderTarget (..), DescriptorSpec (..), PointedDescriptorSet, buf, hasPerDrawDescriptorSet, attributes)
+import Hickory.Vulkan.Types (PostConstants(..), RenderTarget (..), DescriptorSpec (..), PointedDescriptorSet, buf, hasPerDrawDescriptorSet, Material(..))
 import Hickory.Vulkan.Frame (FrameContext(..))
 import Hickory.Vulkan.Text (withMSDFMaterial, MSDFMatConstants (..), TextRenderer)
 import Hickory.Vulkan.Forward.Lit (withStaticUnlitMaterial, withAnimatedLitMaterial, withLitRenderTarget, withStaticLitMaterial, withLineMaterial)
@@ -20,12 +20,10 @@ import Hickory.Vulkan.Mesh (BufferedMesh (..), vsizeOf, vertices, indices)
 import Vulkan (ClearValue (..), ClearColorValue (..), cmdDraw, ClearDepthStencilValue (..), bindings, withDescriptorSetLayout)
 import Foreign (Storable)
 import Hickory.Vulkan.DescriptorSet (withDescriptorSet, BufferDescriptorSet (..), descriptorSetBindings, withDataBuffer, uploadBufferDescriptor, uploadBufferDescriptorArray)
-import Control.Lens (view, has, preview, to, (^.), (.~), (&))
+import Control.Lens (view, (^.), (.~), (&), _1, _2, _3, _4)
 import Hickory.Vulkan.Framing (doubleResource, resourceForFrame, frameResource)
 import Hickory.Vulkan.Material (cmdBindMaterial, cmdPushMaterialConstants, cmdBindDrawDescriptorSet)
-import Data.List (partition)
-import Data.Traversable (for)
-import Data.Maybe (mapMaybe)
+import Data.List (partition, sortOn)
 import Data.Foldable (for_)
 import Hickory.Vulkan.DynamicMesh (DynamicBufferedMesh(..), withDynamicBufferedMesh)
 import qualified Data.Vector as V
@@ -33,10 +31,17 @@ import Vulkan.Zero (zero)
 import Hickory.Vulkan.Forward.ObjectPicking (withObjectIDMaterial, withObjectIDRenderTarget)
 import Linear.Matrix (M44)
 import Hickory.Text (TextCommand(..))
-import Control.Monad (when)
+import Control.Monad (when, unless)
 import Hickory.Types (aspectRatio)
 import Hickory.Camera (shotMatrix, Projection (..))
 import Hickory.Math (viewDirection)
+import Data.Word (Word32)
+import Data.IORef (modifyIORef, newIORef, readIORef, IORef)
+import Data.UUID (UUID)
+import Control.Monad.State.Class (MonadState, put)
+import Control.Monad.State.Strict (evalStateT)
+import qualified Data.UUID as UUID
+import Control.Monad.State.Class (get)
 
 withRenderer :: VulkanResources -> Swapchain -> Acquire Renderer
 withRenderer vulkanResources@VulkanResources {deviceContext = DeviceContext{..}} swapchain = do
@@ -89,6 +94,11 @@ withRenderer vulkanResources@VulkanResources {deviceContext = DeviceContext{..}}
 
   pure Renderer {..}
 
+data RegisteredMaterial const extra
+  = NullMat
+  | Universal (BufferedUniformMaterial const) extra
+  | LitAndUnlit (BufferedUniformMaterial const, extra) (BufferedUniformMaterial const, extra)
+
 renderToRenderer :: (FrameMonad m, MonadIO m) => Renderer -> RenderSettings -> PostConstants -> Command () -> Command () -> m ()
 renderToRenderer Renderer {..} RenderSettings {..} postConstants litF overlayF = do
   frameContext@FrameContext {..} <- askFrameContext
@@ -111,9 +121,12 @@ renderToRenderer Renderer {..} RenderSettings {..} postConstants litF overlayF =
     let drawCommands = runCommand litF
 
     useRenderTarget shadowRenderTarget commandBuffer [ DepthStencil (ClearDepthStencilValue 1 0) ] swapchainImageIndex do
-      let shadowCommands = filter castsShadow drawCommands
-      renderMaterialCommandsAndUpload frameContext staticShadowMaterial   $ selectStaticCommands shadowCommands
-      renderMaterialCommandsAndUpload frameContext animatedShadowMaterial $ selectAnimatedCommands shadowCommands
+      processRenderPass frameContext
+        ( Universal animatedShadowMaterial ()
+        , Universal staticShadowMaterial ()
+        , NullMat
+        , NullMat
+        ) $ filter castsShadow drawCommands
 
     let V4 r g b a = clearColor
     useRenderTarget litRenderTarget commandBuffer
@@ -121,122 +134,177 @@ renderToRenderer Renderer {..} RenderSettings {..} postConstants litF overlayF =
       , DepthStencil (ClearDepthStencilValue 1 0)
       , Color (Float32 1 1 1 1)
       ] swapchainImageIndex do
-      let (blended, opaque) = partition blend drawCommands
-          (litOpaque, unlitOpaque)      = partition lit opaque
-          (litBlended, unlitBlended)    = partition lit blended
-
-      linesUnisOpaque       <- renderMaterialCommands frameContext linesWorldMaterial 0       $ selectLinesCommands opaque
-      staticLitUnisOpaque   <- renderMaterialCommands frameContext staticLitWorldMaterial 0   $ selectStaticCommands litOpaque
-      staticUnlitUnisOpaque <- renderMaterialCommands frameContext staticUnlitWorldMaterial 0 $ selectStaticCommands unlitOpaque
-      animUnisOpaque        <- renderMaterialCommands frameContext animatedLitWorldMaterial 0 $ selectAnimatedCommands litOpaque
-      -- renderMaterialCommands frameContext animatedUnlitWorldMaterial $ selectAnimatedCommands unlitOpaque
-      msdfUnisOpaque         <- renderMaterialCommands frameContext msdfWorldMaterial 0      $ selectMSDFCommands opaque
-
-      linesUnisBlended       <- renderMaterialCommands frameContext linesWorldMaterial (length linesUnisOpaque) $ selectLinesCommands blended
-      staticLitUnisBlended   <- renderMaterialCommands frameContext staticLitWorldMaterial (length staticLitUnisOpaque) $ selectStaticCommands litBlended
-      staticUnlitUnisBlended <- renderMaterialCommands frameContext staticUnlitWorldMaterial (length staticUnlitUnisOpaque) $ selectStaticCommands unlitBlended
-      animUnisBlended        <- renderMaterialCommands frameContext animatedLitWorldMaterial (length animUnisOpaque) $ selectAnimatedCommands blended
-      -- renderMaterialCommands frameContext animatedUnlitWorldMaterial $ selectAnimatedCommands unlitBlended
-      msdfUnisBlended        <- renderMaterialCommands frameContext msdfWorldMaterial (length msdfUnisOpaque) $ selectMSDFCommands blended
-
-      uploadUniformsBuffer frameContext linesWorldMaterial (linesUnisOpaque ++ linesUnisBlended)
-      uploadUniformsBuffer frameContext staticLitWorldMaterial (staticLitUnisOpaque ++ staticLitUnisBlended)
-      uploadUniformsBuffer frameContext staticUnlitWorldMaterial (staticUnlitUnisOpaque ++ staticUnlitUnisBlended)
-      uploadUniformsBuffer frameContext animatedLitWorldMaterial (animUnisOpaque ++ animUnisBlended)
-      uploadUniformsBuffer frameContext msdfWorldMaterial (msdfUnisOpaque ++ msdfUnisBlended)
+      processRenderPass frameContext
+        ( Universal animatedLitWorldMaterial ()
+        , LitAndUnlit (staticLitWorldMaterial, ()) (staticUnlitWorldMaterial, ())
+        , Universal msdfWorldMaterial ()
+        , Universal linesWorldMaterial ()
+        ) drawCommands
 
     useRenderTarget swapchainRenderTarget commandBuffer [] swapchainImageIndex do
       cmdBindMaterial frameNumber commandBuffer postProcessMaterial
       liftIO $ cmdPushMaterialConstants commandBuffer postProcessMaterial postConstants
       liftIO $ cmdDraw commandBuffer 3 1 0 0
 
-      let overlayCommands = runCommand overlayF
-      renderMaterialCommandsAndUpload frameContext staticOverlayMaterial $ selectStaticCommands overlayCommands
-      renderMaterialCommandsAndUpload frameContext msdfOverlayMaterial   $ selectMSDFCommands overlayCommands
+      processRenderPass frameContext
+        ( NullMat
+        , Universal staticOverlayMaterial ()
+        , Universal msdfOverlayMaterial ()
+        , NullMat
+        ) $ runCommand overlayF
+
+type MaterialSet
+  = ( RegisteredMaterial AnimatedConstants ()
+    , RegisteredMaterial StaticConstants ()
+    , RegisteredMaterial MSDFMatConstants ()
+    , RegisteredMaterial StaticConstants ()
+    )
+
+type MaterialConfig c = RegisteredMaterial c (IORef [c])
+
+type MaterialConfigSet
+  = ( MaterialConfig AnimatedConstants
+    , MaterialConfig StaticConstants
+    , MaterialConfig MSDFMatConstants
+    , MaterialConfig StaticConstants
+    )
+
+processRenderPass
+  :: (MonadIO m, DynamicMeshMonad m)
+  => FrameContext
+  -> MaterialSet
+  -> [DrawCommand]
+  -> m ()
+processRenderPass fc mps commands = do
+  let regMatToConfig :: RegisteredMaterial a () -> IO (MaterialConfig a)
+      regMatToConfig = \case
+        NullMat -> pure NullMat
+        Universal mat () -> Universal mat <$> newIORef []
+        LitAndUnlit (mat1,()) (mat2,()) -> LitAndUnlit <$> ((mat1,) <$> newIORef [])
+                                                       <*> ((mat2,) <$> newIORef [])
+  config <- liftIO $
+    (,,,) <$> regMatToConfig (view _1 mps)
+          <*> regMatToConfig (view _2 mps)
+          <*> regMatToConfig (view _3 mps)
+          <*> regMatToConfig (view _4 mps)
+
+  let
+      (blended, opaque) = partition blend commands
+
+  flip evalStateT UUID.nil do
+    -- Sort opaque by material to minimize binding
+    for_ (sortOn identifyDCMat opaque) $ processCommand fc config
+    -- Render blended in order to maintain visual layering
+    for_ blended $ processCommand fc config
+
+  -- IORefs are now bursting full of uniform goodness. Here comes the airplane. Yum!
+  uploadUnis $ view _1 config
+  uploadUnis $ view _2 config
+  uploadUnis $ view _3 config
+  uploadUnis $ view _4 config
   where
-  selectAnimatedCommands :: [DrawCommand] -> [(DrawCommand, (AnimatedConstants, Maybe PointedDescriptorSet))]
-  selectAnimatedCommands = mapMaybe (\c -> (c,) <$> preview (#drawType . #_Animated . to (mkUniform c)) c)
-    where
-    mkUniform DrawCommand {..} AnimatedMesh {..} = (, Just albedo)
-      AnimatedConstants
-        { modelMat  = modelMat
-        , normalMat = transpose . inv33 $ modelMat ^. _m33
-        , color = color
-        , boneMat = boneMat
-        , colors = colors
-        }
-  selectStaticCommands :: [DrawCommand] -> [(DrawCommand, (StaticConstants, Maybe PointedDescriptorSet))]
-  selectStaticCommands = mapMaybe (\c -> (c,) <$> preview (#drawType . #_Static . to (mkUniform c)) c)
-    where
-    mkUniform DrawCommand {..} StaticMesh {..} = (, Just albedo)
-      StaticConstants
-        { modelMat  = modelMat
-        , normalMat = transpose . inv33 $ modelMat ^. _m33
-        , color = color
-        , tiling = tiling
-        }
-  selectLinesCommands :: [DrawCommand] -> [(DrawCommand, (StaticConstants, Maybe PointedDescriptorSet))]
-  selectLinesCommands = mapMaybe (\c -> if has (#drawType . #_Lines) c then Just (c, (mkUniform c, Nothing)) else Nothing)
-    where
-    mkUniform DrawCommand {..} =
-      StaticConstants
-        { modelMat  = modelMat
-        , normalMat = transpose . inv33 $ modelMat ^. _m33
-        , color = color
-        , tiling = V2 1 1
-        }
-  selectMSDFCommands :: [DrawCommand] -> [(DrawCommand, (MSDFMatConstants, Maybe PointedDescriptorSet))]
-  selectMSDFCommands = mapMaybe (\c -> (c,) <$> preview (#drawType . #_MSDF . to (mkUniform c)) c)
-    where
-    mkUniform DrawCommand {..} MSDFMesh {..} = (, Just tex)
-      MSDFMatConstants
-        { modelMat  = modelMat
-        , outlineColor = outlineColor
-        , outlineSize = outlineSize
-        , sdfPixelRange = sdfPixelRange
-        , tiling = tiling
-        , color = color
-        }
+  uploadUnis :: (Storable a, MonadIO m) => MaterialConfig a -> m ()
+  uploadUnis = \case
+    Universal mat ref -> uploadConfig (mat,ref)
+    LitAndUnlit c1 c2 -> uploadConfig c1 >> uploadConfig c2
+    NullMat -> pure ()
+  uploadConfig (mat, ref) = do
+    unis <- liftIO (readIORef ref)
+    unless (null unis) do
+      uploadUniformsBuffer fc mat $ reverse unis
+  identifyDCMat :: DrawCommand -> Int
+  identifyDCMat DrawCommand {..} = (if lit then 2 else 1) * case drawType of
+    Animated _ -> 4
+    Static   _ -> 3
+    MSDF     _ -> 2
+    Lines      -> 1 -- Draw lines first. Might need to be configurable
 
-renderMaterialCommands :: (MonadIO m, DynamicMeshMonad m) => FrameContext -> BufferedUniformMaterial uniform -> Int -> [(DrawCommand, (uniform, Maybe PointedDescriptorSet))] -> m [uniform]
-renderMaterialCommands _ _ _ [] = pure []
-renderMaterialCommands FrameContext {..} BufferedUniformMaterial {..} startIdx commands = do
-  cmdBindMaterial frameNumber commandBuffer material
+processCommand
+  :: (MonadIO m, DynamicMeshMonad m, MonadState UUID m)
+  => FrameContext
+  -> MaterialConfigSet
+  -> DrawCommand
+  -> m ()
+processCommand frameContext
+  ( animatedConfig
+  , staticConfig
+  , msdfConfig
+  , linesConfig
+  )
+  dc@DrawCommand {..} = case drawType of
+  Animated AnimatedMesh {..} -> go animatedConfig $ (,Just albedo) AnimatedConstants
+    { modelMat  = modelMat
+    , normalMat = transpose . inv33 $ modelMat ^. _m33
+    , color = color
+    , boneMat = boneMat
+    , colors = colors
+    }
+  Static StaticMesh {..} -> go staticConfig $ (, Just albedo) StaticConstants
+    { modelMat  = modelMat
+    , normalMat = transpose . inv33 $ modelMat ^. _m33
+    , color = color
+    , tiling = tiling
+    }
+  Lines -> go linesConfig $ (, Nothing) StaticConstants
+    { modelMat  = modelMat
+    , normalMat = transpose . inv33 $ modelMat ^. _m33
+    , color = color
+    , tiling = V2 1 1
+    }
+  MSDF MSDFMesh {..} -> go msdfConfig $ (, Just tex) MSDFMatConstants
+    { modelMat  = modelMat
+    , outlineColor = outlineColor
+    , outlineSize = outlineSize
+    , sdfPixelRange = sdfPixelRange
+    , tiling = tiling
+    , color = color
+    }
 
-  for (zip commands [fromIntegral startIdx..]) \((DrawCommand {..}, (uniform, drawDS)), i) -> do
-    cmdPushMaterialConstants commandBuffer material i
-    when (hasPerDrawDescriptorSet material) do
-      for_ drawDS $ cmdBindDrawDescriptorSet commandBuffer material
-    case mesh of
-      Buffered BufferedMesh {mesh = mesh', ..} -> cmdDrawBufferedMesh commandBuffer material mesh' 0 vertexBuffer 0 indexBuffer
-      Dynamic dyn -> do
-        meshes <- getMeshes
-        addMesh dyn
+  where
+  go c (uniform, ds) = do
+    let config = case c of
+          Universal mat ref -> (mat, ref)
+          LitAndUnlit c1 c2 -> if lit then c1 else c2
+          NullMat -> error "No mat registered to handle this draw command"
+    unis <- liftIO $ readIORef (snd config)
+    renderCommand frameContext (fst config) dc ds (fromIntegral $ length unis)
+    liftIO $ modifyIORef (snd config) (uniform:)
 
-        -- This is O(n)... Might want to cache this
-        let vertexSizeThusFar = sum $ map (sum . map (vsizeOf . snd) . vertices) meshes
-            indexSizeThusFar  = sum $ map (maybe 0 vsizeOf . indices) meshes
+renderCommand
+  :: (MonadIO m, DynamicMeshMonad m, MonadState UUID m)
+  => FrameContext
+  -> BufferedUniformMaterial uniform
+  -> DrawCommand
+  -> Maybe PointedDescriptorSet
+  -> Word32
+  -> m ()
+renderCommand FrameContext {..} BufferedUniformMaterial {..} DrawCommand {..} drawDS uniformIdx = do
+  curUUID <- get
+  when (curUUID /= uuid material) do
+    cmdBindMaterial frameNumber commandBuffer material
+    put curUUID
 
-        DynamicBufferedMesh { vertexBufferPair = (vertexBuffer,_), indexBufferPair = (indexBuffer,_) } <- askDynamicMesh
-        cmdDrawBufferedMesh commandBuffer material dyn vertexSizeThusFar vertexBuffer (fromIntegral indexSizeThusFar) (Just indexBuffer)
+  cmdPushMaterialConstants commandBuffer material uniformIdx
+  when (hasPerDrawDescriptorSet material) do
+    for_ drawDS $ cmdBindDrawDescriptorSet commandBuffer material
+  case mesh of
+    Buffered BufferedMesh {mesh = mesh', ..} -> cmdDrawBufferedMesh commandBuffer material mesh' 0 vertexBuffer 0 indexBuffer
+    Dynamic dyn -> do
+      meshes <- getMeshes
+      addMesh dyn
 
-    pure uniform
+      -- This is O(n)... Might want to cache this
+      let vertexSizeThusFar = sum $ map (sum . map (vsizeOf . snd) . vertices) meshes
+          indexSizeThusFar  = sum $ map (maybe 0 vsizeOf . indices) meshes
+
+      DynamicBufferedMesh { vertexBufferPair = (vertexBuffer,_), indexBufferPair = (indexBuffer,_) } <- askDynamicMesh
+      cmdDrawBufferedMesh commandBuffer material dyn vertexSizeThusFar vertexBuffer (fromIntegral indexSizeThusFar) (Just indexBuffer)
 
 uploadUniformsBuffer :: (MonadIO m, Storable a) => FrameContext -> BufferedUniformMaterial a -> [a] -> m ()
 uploadUniformsBuffer FrameContext {..} BufferedUniformMaterial {..} uniforms = do
   let BufferDescriptorSet { dataBuffer } = resourceForFrame frameNumber descriptor
 
   uploadBufferDescriptorArray dataBuffer uniforms
-
-renderMaterialCommandsAndUpload
-  :: (MonadIO m, DynamicMeshMonad m, Storable a)
-  => FrameContext
-  -> BufferedUniformMaterial a
-  -> [(DrawCommand, (a, Maybe PointedDescriptorSet))]
-  -> m ()
-renderMaterialCommandsAndUpload frameContext mat commands
-  = renderMaterialCommands frameContext mat 0 commands
-  >>= uploadUniformsBuffer frameContext mat
 
 drawText
   :: CommandMonad m
