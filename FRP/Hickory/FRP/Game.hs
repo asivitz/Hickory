c57034b5c5c093391f9736aa419e3ada29825184
Axis Sivitz <aosivitz@gmail.com>
2022-12-23 22:22:26 -0500

    Game loop w/ all the bells and whistles

--- FRP/Hickory/FRP/Game.hs
+++ FRP/Hickory/FRP/Game.hs
@@ -1,10 +1,26 @@
+{-# LANGUAGE RecursiveDo #-}
+
 module Hickory.FRP.Game where
 
 import Hickory.FRP.Combinators (unionFirst)
 import Data.Time (NominalDiffTime)
 import qualified Reactive.Banana as B
 import qualified Reactive.Banana.Frameworks as B
-import Hickory.Math (Scalar)
+import Hickory.Math (Scalar, Interpolatable (glerp))
+import Hickory.Vulkan.Forward.Types (Renderer)
+import Hickory.FRP.CoreEvents (CoreEvents (..))
+import Hickory.Vulkan.Types (FrameContext)
+import Linear (V2(..))
+import Hickory.Types (Size(..))
+import Control.Lens (view, _1)
+import Reactive.Banana ((<@>))
+import Data.Maybe (maybeToList)
+import Data.List (nub)
+import Data.Bool (bool)
+import Hickory.Input (Key(..))
+import Hickory.Vulkan.Forward.Renderer (pickObjectID)
+import Control.Monad.IO.Class (liftIO)
+import Hickory.FRP.Historical (historicalWithEvents)
 
 -- Queue up events and release in a batch
 -- For example, to collect a frame's worth of input events and process at
@@ -44,3 +60,71 @@ loadOrStep eLoad eStep = unionFirst
   [ eStep
   , const <$> ((,[]) <$> eLoad)
   ]
+
+type Step input gameState gameEvent = (NominalDiffTime, [input]) -> gameState -> (gameState, [gameEvent])
+
+-- This game loop features
+-- - Pausing
+-- - Rewind/Forward
+-- - Interpolating game state (for less frequent logic updates)
+-- - Selecting objects in pause mode (for debugging)
+-- TODO: Ideally, screen picking is supported through an iface,
+-- rather than using a specific renderer
+gameNetwork
+  :: forall gameState input gameEvent
+   . Interpolatable gameState
+  => NominalDiffTime
+  -> CoreEvents (Renderer, FrameContext)
+  -> gameState
+  -> B.Event gameState
+  -> B.Event [input]
+  -> B.Behavior (Step input gameState gameEvent)
+  -> B.MomentIO (B.Behavior gameState, B.Event [gameEvent], B.Behavior [Int])
+gameNetwork logicTimeStep coreEvents initialState eLoadState eInput step = mdo
+  let frameSelect =
+        unionFirst [ 1 <$ keyDownOrHeld coreEvents Key'Left
+                   , 5 <$ keyDownOrHeld coreEvents Key'LeftBracket
+                   , (-1) <$ keyDownOrHeld coreEvents Key'Right
+                   , (-5) <$ keyDownOrHeld coreEvents Key'RightBracket
+                   ]
+
+  paused <- B.accumB False $ not <$ B.filterE (== Key'Space) (keyDown coreEvents)
+
+  replayQueue :: B.Behavior [gameState] <- B.accumB [] $ B.unions
+    [ tail <$ B.whenE bShowingReplay eInput
+    -- fmap const . buildDebugReplay <$> params <*> mdl <@ B.filterE (== Key'J) (keyDown coreEvents)
+    ]
+
+  let bShowingReplay = not . null <$> replayQueue
+
+  (frameFraction, eFrameInput) <- do
+    timeStep logicTimeStep eInput $
+      unionFirst [ B.whenE bShowingReplay $ (/4) <$> eTime coreEvents
+                 , B.whenE (not <$> bShowingReplay) $ B.whenE (not <$> paused) (eTime coreEvents)
+                 ]
+
+  (currentGDPair, gameEvs) <- historicalWithEvents
+    initialState
+    (loadOrStep eLoadState (step <@> B.whenE (not <$> bShowingReplay) eFrameInput))
+    frameSelect
+
+  let replayGDPair = (\case
+        x1:x2:_ -> (x1, x2)
+        x:_ -> (x,x)
+        [] -> (error "No replay data", error "Er")
+        ) <$> replayQueue
+      gdPair = bool <$> currentGDPair <*> replayGDPair <*> bShowingReplay
+
+      gd  = uncurry . glerp <$> frameFraction <*> gdPair
+
+  let eClick = (\(Size w h) (_, V2 x y, _) -> (x/ realToFrac w, y/ realToFrac h))
+           <$> scrSizeB coreEvents
+           <@> fmap head (B.filterE ((<0.3) . view _1 . head) $ eTouchesUp coreEvents)
+  renInfo <- B.stepper undefined (eRender coreEvents)
+  eScreenPickedObjectID <- fmap (\x -> if x > 0 then Just x else Nothing) <$> B.execute (((\(r,fc) -> liftIO . pickObjectID fc r) <$> renInfo) <@> eClick)
+  selectedIds <- B.accumB [] $ unionFirst
+    [ maybe id (\x -> nub . (x:)) <$> B.whenE (keyHeldB coreEvents Key'LeftShift) eScreenPickedObjectID
+    , const . maybeToList <$> eScreenPickedObjectID
+    ]
+
+  pure (gd, gameEvs, selectedIds)
