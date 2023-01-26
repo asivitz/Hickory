6cf524f910bd6ce3d4fd97648162964a041238bd
Axis Sivitz <aosivitz@gmail.com>
2023-01-26 15:40:29 -0500

    Add an integral for cubic bezier

--- core/Hickory/Math/Bezier.hs
+++ core/Hickory/Math/Bezier.hs
@@ -23,7 +23,7 @@ cubic start control1 control2 end t
   omt_sq = oneminust * oneminust
   omt_cu = omt_sq * oneminust
 
--- |This is the derivative of the `cubic` function
+-- |This is the derivative of the 'cubic' function
 cubicDerivative :: (Functor f, Num a, Num (f a))
       => f a
       -> f a
@@ -45,3 +45,25 @@ cubicDerivative start control1 control2 end t
   omt_sq' = 2 * oneminust * omt'
   omt_cu' = 3 * omt_sq * omt'
   omt' = -1
+
+-- |This is the integral of the 'cubic' function
+cubicIntegral :: (Functor f, Num a, Num (f a), Fractional a)
+      => f a
+      -> f a
+      -> f a
+      -> f a
+      -> a
+      -> f a
+cubicIntegral start control1 control2 end t
+  = start ^* (omt_qua * (-1/4))
+  + control1 ^* (3 * (tqua/4 - tcu * (2/3) + tsq/2))
+  + control2 ^* (3 * (tcu/3 - tqua/4))
+  + end ^* (tqua/4)
+  where
+  tsq = t * t
+  tcu = tsq * t
+  tqua = tcu * t
+  oneminust = 1 - t
+  omt_sq = oneminust * oneminust
+  omt_cu = omt_sq * oneminust
+  omt_qua = omt_cu * oneminust
