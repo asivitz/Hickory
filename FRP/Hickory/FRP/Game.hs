54200b8072eafd96da78e1b30914bc19dbcd909f
Axis Sivitz <aosivitz@gmail.com>
2023-08-14 16:30:47 -0400

    Game network supports slow motion replay

--- FRP/Hickory/FRP/Game.hs
+++ FRP/Hickory/FRP/Game.hs
@@ -12,7 +12,7 @@ import Hickory.FRP.CoreEvents (CoreEvents (..))
 import Hickory.Vulkan.Types (FrameContext)
 import Linear (V2(..))
 import Hickory.Types (Size(..))
-import Reactive.Banana ((<@>))
+import Reactive.Banana ((<@>), (<@))
 import Data.Maybe (maybeToList)
 import Data.List (nub)
 import Data.Bool (bool)
@@ -22,6 +22,8 @@ import Control.Monad.IO.Class (liftIO)
 import Hickory.FRP.Historical (historicalWithEvents)
 import Hickory.Camera (Camera)
 import GHC.Generics (Generic)
+import qualified Data.Sequence as S
+import Data.Fixed (mod')
 
 -- Queue up events and release in a batch
 -- For example, to collect a frame's worth of input events and process at
@@ -62,7 +64,7 @@ type Step input gameState gameEvent = (NominalDiffTime, [input]) -> gameState ->
 
 -- This game loop features
 -- - Pausing
--- - Rewind/Forward
+-- - Rewind/Forward, slow motion replay
 -- - Interpolating game state (for less frequent logic updates)
 -- - Selecting objects in pause mode (for debugging)
 -- TODO: Ideally, screen picking is supported through an iface,
@@ -81,13 +83,40 @@ gameNetwork
   -> B.MomentIO (B.Behavior gameState, B.Event [gameEvent], B.Behavior Bool)
 gameNetwork logicTimeStep pauseKey coreEvents initialState eLoadState eInput step eReplay = mdo
   let frameSelect = B.whenE paused $
-        unionFirst [ 1 <$ keyDownOrHeld coreEvents Key'Left
-                   , 5 <$ keyDownOrHeld coreEvents Key'LeftBracket
-                   , (-1) <$ keyDownOrHeld coreEvents Key'Right
-                   , (-5) <$ keyDownOrHeld coreEvents Key'RightBracket
+        unionFirst [ (+1) <$ keyDownOrHeld coreEvents Key'Left
+                   , (+5) <$ keyDownOrHeld coreEvents Key'LeftBracket
+                   , subtract 1 <$ keyDownOrHeld coreEvents Key'Right
+                   , subtract 5 <$ keyDownOrHeld coreEvents Key'RightBracket
+                   , const 0 <$ eFlipPause
                    ]
 
-  paused <- B.accumB False $ not <$ B.filterE (== pauseKey) (keyDown coreEvents)
+  bAuto <- B.accumB False $ B.unions
+    [ not <$ B.filterE (== Key'S) (keyDown coreEvents)
+    , const False <$ eFlipPause
+    ]
+
+  bSpeedFactor <- B.stepper 1 . B.whenE paused $ unionFirst
+    [ 1 <$ B.filterE (== Key'0) (keyDown coreEvents)
+    , 0.01 <$ B.filterE (== Key'1) (keyDown coreEvents)
+    , 0.03 <$ B.filterE (== Key'2) (keyDown coreEvents)
+    , 0.07 <$ B.filterE (== Key'3) (keyDown coreEvents)
+    , 0.1 <$ B.filterE (== Key'4) (keyDown coreEvents)
+    , 0.2 <$ B.filterE (== Key'5) (keyDown coreEvents)
+    , 0.3 <$ B.filterE (== Key'6) (keyDown coreEvents)
+    , 0.4 <$ B.filterE (== Key'7) (keyDown coreEvents)
+    , 0.5 <$ B.filterE (== Key'8) (keyDown coreEvents)
+    , 0.7 <$ B.filterE (== Key'9) (keyDown coreEvents)
+    ]
+
+  bCurrentFrameSelect <- B.accumB 0 $ B.unions
+    [ frameSelect
+    , B.whenE bAuto $ (\factor time acc -> max 0 (acc - realToFrac time * factor / realToFrac logicTimeStep)) <$> bSpeedFactor <@> eTime coreEvents
+    ]
+
+  let eFlipPause = B.filterE (== pauseKey) (keyDown coreEvents)
+      eUnpause = B.whenE paused eFlipPause
+
+  paused <- B.accumB False $ not <$ eFlipPause
 
   replayQueue :: B.Behavior [gameState] <- B.accumB [] $ B.unions
     [ tail <$ B.whenE bShowingReplay eFrameInput
@@ -102,21 +131,26 @@ gameNetwork logicTimeStep pauseKey coreEvents initialState eLoadState eInput ste
                  , B.whenE (not <$> bShowingReplay) $ B.whenE (not <$> paused) (eTime coreEvents)
                  ]
 
-  (currentGDPair, gameEvs) <- historicalWithEvents
+  (history, gameEvs) <- historicalWithEvents
     initialState
     (B.unionWith const (const <$> ((,[]) <$> eLoadState)) (step <@> B.whenE (not <$> bShowingReplay) eFrameInput))
-    frameSelect
+    ((\f -> f <$> bCurrentFrameSelect <@ eUnpause) (S.drop . floor))
 
+  let bCurrentFrameFraction = (\f -> f <$> frameFraction <*> bCurrentFrameSelect <*> paused) \frac frame p ->
+        if p then 1 - (frame `mod'` 1) else frac
+      actualGDPair = (\f -> f <$> history <*> bCurrentFrameSelect) \s (floor -> i) ->
+        let from = S.index s (max 0 (min (i + 1) (S.length s - 1)))
+            to = S.index s (max 0 (min (S.length s - 1) i))
+        in (from, to)
 
-  let replayGDPair = (\case
+      replayGDPair = (\case
         x1:x2:_ -> (x1, x2)
         x:_ -> (x,x)
         [] -> (error "No replay data", error "Er")
         ) <$> replayQueue
-      gdPair = bool <$> currentGDPair <*> replayGDPair <*> bShowingReplay
+      gdPair = bool <$> actualGDPair <*> replayGDPair <*> bShowingReplay
 
-      gd  = uncurry . glerp <$> frameFraction <*> gdPair
-
+      gd  = uncurry . glerp <$> bCurrentFrameFraction <*> gdPair
 
   pure (gd, gameEvs, paused)
 
--- FRP/Hickory/FRP/Historical.hs
+++ FRP/Hickory/FRP/Historical.hs
@@ -5,43 +5,40 @@ module Hickory.FRP.Historical
   where
 
 import Hickory.FRP.Combinators (unionFirst)
-import Reactive.Banana (Behavior, Event, MonadMoment, mapAccum)
+import Reactive.Banana (Behavior, Event, MonadMoment, mapAccum, never)
 import qualified Data.Sequence as S
 
-historical :: MonadMoment m => a -> Event (a -> a) -> Event Int -> m (Behavior (a,a))
-historical initial eStep eChangeIndex =
-  fst <$> historicalWithEvents initial (((,[]) .) <$> eStep) eChangeIndex
+historical :: MonadMoment m => a -> Event (a -> a) -> m (Behavior a)
+historical initial eStep = do
+  (history, _) <- historicalWithEvents initial (((,[]) .) <$> eStep) never
+  pure $
+    (\s -> case S.viewl s of
+      S.EmptyL -> error "Shouldn't happen"
+      x S.:< _ -> x
+    ) <$> history
 
 -- Evolve a state according to a step function, while collecting events
 historicalWithEvents
   :: MonadMoment m
   => a -- Initial state
   -> Event (a -> (a,[b])) -- Step function producing a new state and some 'events'
-  -> Event Int -- Change current state by relative index (e.g. -1 means go back 1 frame)
-  -> m ( Behavior (a,a) -- Previous and current states
+  -> Event (S.Seq a -> S.Seq a) -- Modify the history
+  -> m ( Behavior (S.Seq a)
        , Event [b]      -- New events
        )
-historicalWithEvents initial eStep eChangeIndex = do
-  (evs, history) <- mapAccum (S.singleton initial, 0) $
+historicalWithEvents initial eStep eModify = do
+  (evs, history) <- mapAccum (S.singleton initial) $
     unionFirst [ append <$> eStep
-               , changeIdx <$> eChangeIndex
+               , (\f s -> ([], f s)) <$> eModify
                ]
-  pure (assemble <$> history, evs)
-  where
-  assemble (s,i) = let from = S.index s (min (i + 1) (S.length s - 1))
-                       to = S.index s i
-                   in (from, to)
+  pure (history, evs)
 
 maxLen :: Int
 maxLen = 500
 
-changeIdx :: Int -> (S.Seq a1, Int) -> ([a2], (S.Seq a1, Int))
-changeIdx delta (s, i) = ([], (s, min (max 0 (i + delta)) (S.length s - 1)))
-
-append :: Num b => (a1 -> (a1, a2)) -> (S.Seq a1, Int) -> (a2, (S.Seq a1, b))
-append stepF (rest, i) =
-  let rst  = if i > 0 then S.drop i rest else rest
-      rst' = if not (null rst) then S.take maxLen rst else rst
-  in  case S.viewl rst' of
+append :: (a1 -> (a1, a2)) -> S.Seq a1 -> (a2, S.Seq a1)
+append stepF history =
+  let history' = if not (null history) then S.take maxLen history else history
+  in  case S.viewl history' of
         S.EmptyL -> error "Shouldn't happen"
-        x S.:< _ -> let (acc, evs) = stepF x in (evs, (acc S.<| rst', 0))
+        x S.:< _ -> let (acc, evs) = stepF x in (evs, acc S.<| history')
