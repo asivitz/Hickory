8a14dfd675b3cc83501937b6064db53a5274203b
Axis Sivitz <aosivitz@gmail.com>
2024-03-26 13:16:45 -0400

    Cleanup some old input code

--- GLFW/Platforms/GLFW.hs
+++ GLFW/Platforms/GLFW.hs
@@ -6,9 +6,5 @@ import Hickory.Types (Size)
 import qualified Graphics.UI.GLFW as GLFW
 import qualified Platforms.GLFW.Bridge as Bridge
 
-makeGLFWInputPoller :: GLFW.Window -> IO (IO [RawInput])
-makeGLFWInputPoller win = makeInputPoller (Bridge.setupInput win)
-
-
 getGLFWWindowSizeRef :: GLFW.Window -> IO (IORef (Size Int))
 getGLFWWindowSizeRef = Bridge.getWindowSizeRef
