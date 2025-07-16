9286ece60c645a0cf901a4483f5959c2b653500b
Axis Sivitz <aosivitz@gmail.com>
2025-07-16 14:48:43 -0400

    TH test

--- small/Small/Foo.hs
+++ small/Small/Foo.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE QuasiQuotes, TemplateHaskell #-}
 
 module Small.Foo where
 
 import Data.String.QM (qt)
 import Data.Text (Text)
+import Small.QQ (frag)
+
+import Vulkan.Utils.ShaderQQ.GLSL.Glslang (compileShaderQ)
 
 foo :: Int
 foo = 1
@@ -12,3 +15,5 @@ bar :: Text
 bar = [qt|
 bartext
 |]
+
+compiled = $(compileShaderQ Nothing "frag" Nothing frag)
