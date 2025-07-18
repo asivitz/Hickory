e11266b5d0f5195595856aa1d8eb9e305b755d26
Axis Sivitz <aosivitz@gmail.com>
2025-07-18 15:58:03 -0400

    Curious about this one

--- small/hackory/Hickory/ImGUI.hs
+++ small/hackory/Hickory/ImGUI.hs
@@ -6,16 +6,7 @@ import qualified Foreign.C.String as Text
 import Control.Exception (bracket)
 import GHC.Ptr (nullPtr)
 import Linear (V3 (..), V4 (..), V2 (..))
-import Control.Lens (iso, Iso')
 
-
-myWithWindow :: String -> IO (Maybe a) -> IO (Maybe a)
-myWithWindow name action = bracket (myBegin name) (const Raw.end) (\b -> if b then action else pure Nothing)
-
-myBegin :: String -> IO Bool
-myBegin name =
-  Text.withCString name \namePtr ->
-    Raw.begin namePtr (Just nullPtr) (Just Raw.ImGuiWindowFlags_MenuBar)
 
 imVec3ToColor :: ImVec3 -> V4 Float
 imVec3ToColor (ImVec3 x y z) = V4 x y z 1
@@ -55,12 +46,3 @@ v2ToTuple (V2 r g) = (r,g)
 
 instance Eq ImVec4 where
   a == b = imVec4ToV4 a == imVec4ToV4 b
-
-v3ImVec3Iso :: Iso' (V3 Float) ImVec3
-v3ImVec3Iso = iso v3ToImVec3 imVec3ToV3
-
-v3TripleIso :: Iso' (V3 Float) (Float,Float,Float)
-v3TripleIso = iso v3ToTriple tripleToV3
-
-v4ImVec4Iso :: Iso' (V4 Float) ImVec4
-v4ImVec4Iso = iso v4ToImVec4 imVec4ToV4
