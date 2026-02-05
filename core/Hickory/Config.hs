008d8838c7c60ff2d4b7f7c4eb6efde1ff4531e5
Axis Sivitz <aosivitz@gmail.com>
2026-02-05 17:57:13 -0500

    Need to recursively parse fields w/ default machinery

--- core/Hickory/Config.hs
+++ core/Hickory/Config.hs
@@ -8,20 +8,19 @@ module Hickory.Config where
 
 import GHC.Generics
 import qualified Data.Yaml as Yaml
-import Data.Yaml (Value (..), FromJSON, withObject, (.:?))
+import Data.Yaml (Value (..), withObject, (.:?))
 import Data.Default (Default(..))
 import Data.Text (pack, Text)
 import Data.ByteString (ByteString, readFile)
 import Data.Aeson.Key (fromText)
 import Data.Bifunctor (first)
-import Data.Maybe (fromMaybe)
 
-parseYamlDefault :: (Generic a, GFromYamlDefault (Rep a)) => ByteString -> Either String a
+parseYamlDefault :: FromYamlDefault a => ByteString -> Either String a
 parseYamlDefault bs = do
   value <- first show $ Yaml.decodeEither' bs
   Yaml.parseEither parseJSONWithDefault value
 
-readYamlDefault :: (Generic a, GFromYamlDefault (Rep a)) => FilePath -> IO (Either String a)
+readYamlDefault :: FromYamlDefault a => FilePath -> IO (Either String a)
 readYamlDefault = fmap parseYamlDefault . Data.ByteString.readFile
 
 class FromYamlDefault a where
@@ -32,11 +31,6 @@ class FromYamlDefault a where
     , GFromYamlDefault (Rep a)
     ) => Value -> Yaml.Parser a
   parseJSONWithDefault val = to <$> gParseJsonWithDefault val
-
-instance
-  ( Generic a
-  , GFromYamlDefault (Rep a)
-  ) => FromYamlDefault a
 
 -- Generic machinery to parse using yaml instances with the added feature
 -- that missing fields are filled with default values (from Data.Default)
@@ -50,7 +44,7 @@ instance (GFromYamlDefault a, GFromYamlDefault b) => GFromYamlDefault (a :*: b) 
   gParseJsonWithDefault val = do
     aVal <- gParseJsonWithDefault val
     bVal <- gParseJsonWithDefault val
-    return (aVal :*: bVal)
+    pure (aVal :*: bVal)
 
 -- Metadata for the whole datatype (M1 D)
 instance GFromYamlDefault f => GFromYamlDefault (M1 D d f) where
@@ -61,10 +55,13 @@ instance GFromYamlDefault f => GFromYamlDefault (M1 C c f) where
   gParseJsonWithDefault val = M1 <$> gParseJsonWithDefault val
 
 -- Metadata for a selector (i.e. a single field) (M1 S)
-instance (Selector s, Default c, FromJSON c) => GFromYamlDefault (M1 S s (K1 i c)) where
+instance (Selector s, Default c, FromYamlDefault c) => GFromYamlDefault (M1 S s (K1 i c)) where
   gParseJsonWithDefault = withObject "record" \obj -> do
     let fieldName :: Text
         fieldName = pack (selName (undefined :: M1 S s (K1 i c) p))
 
-    mVal <- obj .:? fromText fieldName
-    return $ M1 (K1 (fromMaybe def mVal))
+    mv <- obj .:? fromText fieldName
+    v <- case mv of
+      Just v -> parseJSONWithDefault v
+      Nothing -> pure def
+    pure $ M1 (K1 v)
